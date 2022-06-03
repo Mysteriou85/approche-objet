1bfeba2daff96758ff25d4e6bd3f7b4e49f20464
Mysteriou85 <guib5@hotmail.fr>
2022-06-03 16:21:23 +0200

    tp04

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,22 @@
+package utils;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class ConversionNombre {
+
+    public static void main(String[] args) {
+
+        String chaine = "12";
+
+        int nombre = Integer.parseInt(chaine);
+
+        int a = 5;
+        int b = 10;
+
+        int c = Integer.max(a, b);
+
+        System.out.println(nombre);
+        System.out.println(c);
+    }
+
+}
