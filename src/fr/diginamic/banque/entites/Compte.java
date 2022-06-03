f2d34eb6feb00a57fb88cb62f8cc9e483970ecf1
Mysteriou85 <guib5@hotmail.fr>
2022-06-03 16:30:04 +0200

    tp05

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,12 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+
+    int numeroCompte;
+    int soldeCompte;
+
+    public Compte(int numeroCompte, int soldeCompte) {
+        this.numeroCompte = numeroCompte;
+        this.soldeCompte = soldeCompte;
+    }
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,11 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+    public static void main(String[] args) {
+        Compte compte1 = new Compte(1, 15);
+
+        System.out.println(compte1);
+    }
+
+}
