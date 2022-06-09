3b0d310cd7e42a1425cac195a26048b0f42f546d
Mysteriou85 <guib5@hotmail.fr>
2022-06-09 15:03:22 +0200

    TP 15 - (Fichier) edit1

--- src/fichier/ManipulationFichier.java
+++ src/fichier/ManipulationFichier.java
@@ -6,6 +6,7 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,6 +15,8 @@ public class ManipulationFichier {
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("D:/project/Projet IntelliJ IDEA/approche-objet/src/fichier/recensement.csv");
         Path newPath = Paths.get("D:/project/Projet IntelliJ IDEA/approche-objet/src/fichier/newRecensement.csv");
+        Path sortie = Paths.get("D:/project/Projet IntelliJ IDEA/approche-objet/src/fichier/Recensement(Ville25000).csv");
+        Files.deleteIfExists(sortie);
         System.out.println(path.getRoot());
         System.out.println(path.getParent());
         System.out.println(path.getFileName());
@@ -32,17 +35,23 @@ public class ManipulationFichier {
 
         Integer nombreHabitant;
 
+        List<String> outputLines = new ArrayList<>();
+
         for (String line : lines) {
             String[] value = line.split(";");
             value[7] = value[7].trim().replaceAll(" ", "");
             if (lines.indexOf(line) !=0) {
                 nombreHabitant = Integer.parseInt(value[7]);
+                String codeDepartement = value[2];
+                String nomVille = value[6];
 
                 if (nombreHabitant > 25000) {
-                    System.out.println("Ville : " + value[6] + " Population : " + value[7]);
+                    // System.out.println("Ville : " + value[6] + " Population : " + value[7]);
+                    outputLines.add(nomVille + ";" + codeDepartement + ";" + nombreHabitant);
                 }
             }
         }
+        Files.write(sortie, outputLines);
 
     }
 
